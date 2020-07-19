c398ba8e01b0cb709cbd5760c3ccb69a3196405d
adamigz <pitrowskiadamm@gmail.com>
2020-07-19 16:25:57 +0200

    Alpha User component (not working properly)

--- src/views/User.vue
+++ src/views/User.vue
@@ -13,43 +13,19 @@
         </section>
         <section class="section section-skew">
             <div class="container">
-                <card shadow class="card-profile mt--300" no-body>
+                <card shadow class="card-profile mt--500" no-body>
                     <div class="px-4">
                         <div class="row justify-content-center">
-                            <div class="col-lg-3 order-lg-2">
-                                <div class="card-profile-image">
-                                    <a href="#">
-                                        <img v-lazy="'img/theme/team-4-800x800.jpg'" class="rounded-circle">
-                                    </a>
+                            
+                            <div class="col-lg-3">
+                                <div class="card-profile-image mb-5">
+                                    <img src="/img/icons/common/profile.jpg" class="rounded-circle">
                                 </div>
                             </div>
-                            <div class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center">
-                                <div class="card-profile-actions py-4 mt-lg-0">
-                                    <base-button type="info" size="sm" class="mr-4">Connect</base-button>
-                                    <base-button type="default" size="sm" class="float-right">Message</base-button>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 order-lg-1">
-                                <div class="card-profile-stats d-flex justify-content-center">
-                                    <div>
-                                        <span class="heading">22</span>
-                                        <span class="description">Friends</span>
-                                    </div>
-                                    <div>
-                                        <span class="heading">10</span>
-                                        <span class="description">Photos</span>
-                                    </div>
-                                    <div>
-                                        <span class="heading">89</span>
-                                        <span class="description">Comments</span>
-                                    </div>
-                                </div>
-                            </div>
+                          
                         </div>
                         <div class="text-center mt-5">
-                            <h3>Jessica Nowak
-                                <span class="font-weight-light">, 27</span>
-                            </h3>
+                            <h3>{{user.name}}</h3>
                             <div class="h6 font-weight-300"><i class="ni location_pin mr-2"></i>Bucharest, Romania</div>
                             <div class="h6 mt-4"><i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer</div>
                             <div><i class="ni education_hat mr-2"></i>University of Computer Science</div>
@@ -69,7 +45,32 @@
     </div>
 </template>
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            user: {},
+            loading: true,
+            loaded: false,
+            errorMsgs: [],
+            showError: false
+        }
+    },
+    computed: {
+        userId() {
+            return this.$route.params.id;
+        }
+    },
+    methods: {
+        async getUser() {
+            let {data} = await this.axios('https://jsonplaceholder.typicode.com/users/'+this.userId);
+            this.user = data;
+        }
+    },
+    async created() {
+        await this.getUser();
+        console.log(this.user);
+    }
+};
 </script>
 <style>
 </style>
